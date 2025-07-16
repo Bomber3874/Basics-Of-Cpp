3157e5ff8ec92b32d017959c7cb5b3ec05460dd4
Bomber3874 <sanidhyasingh1feb@gmail.com>
2025-07-16 09:43:57 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout<<"Enter first number: \n";
+    cin >> a;
+    cout<<"Enter second number: \n";
+    cin >> b;
+    int sum=a+b,subs=a-b,mult=a*b;
+    float divi=a/b;
+    cout<<"Addition: ";
+    cout<<sum;
+    cout<<"\nSubtraction: ";
+    cout<<subs;
+    cout<<"\nMultiplication: ";
+    cout<<mult;
+    cout<<"\nDivision: ";
+    cout<<divi;
+    return 0;
+}
+
+/*Output
+Enter first number: 
+20
+Enter second number: 
+30
+Addition: 50
+Subtraction: -10
+Multiplication: 600
+Division: 0
+*/
---
+++ Input-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout<<"Enter first number: \n";
+    cin >> a;
+    cout<<"Enter second number: \n";
+    cin >> b;
+    return 0;
+}
+
+/*Output
+Enter first number: 
+1
+Enter second number: 
+2
+*/
